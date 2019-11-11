8ecd8d4245a040838252d05cab5de426788ba0d6
SantiRod <santiago.rodriguez@ing.unlp.edu.ar>
2019-11-11 19:54:23 -0300

    para probar: pwm a 25 khz, motores con zona muerta, ....

--- src/seguidor/main/main.ino
+++ src/seguidor/main/main.ino
@@ -1,15 +1,16 @@
-#define KP 1  
-#define KD 2.5	// Probar con KD >> KP 
+#define KP 1
+#define KD 2.5	          // Probar con KD >> KP 
 #define VEL_MAX 150
-#define VEL VEL_MAX/3*2// con 200
-#define DT 1000  // Cada 1 ms accion de control.
+#define VEL VEL_MAX/3*2   // con 200
+#define PWM_DEAD_ZONE 75  // los motores recien se mueven en 75
+#define DT 1000           // Cada 1 ms accion de control. (ESTO HAY QUE CAMBIARLO POR EL WHILE)
 
 #define M1PWM 10
 #define M1A 8
 #define M2PWM 9
 #define M2A 7
-#define LED1 12
-#define LED2 11
+#define LED1 12 // color rojo
+#define LED2 11 // color verde
 
 #define S1 A0
 #define S2 A2
@@ -20,10 +21,11 @@
 
 #define DT2 50000 // 200000
 
-#define BOTON 6
+#define BOT_START 5
+#define LLAVE 6
 
-// TIME 
-unsigned long dt = 0; 
+// TIME
+unsigned long dt = 0;
 unsigned long Now = 0;
 unsigned long LastTime[1] = {0};
 unsigned long LastTime2[1] = {0};
@@ -31,7 +33,7 @@ unsigned long LastTime2[1] = {0};
 // CONTROLLER
 float LastError[1] = {0};
 float Error[1] = {0};
-float Constantes[4] = {KP,KD,DT,VEL};
+float Constantes[4] = {KP, KD, DT, VEL};
 int Vel_filtrada[1] = {0};
 
 // SENSORES
@@ -41,58 +43,73 @@ boolean Ledon_state = false;
 byte Flags[1] = {0};
 boolean Flag = false;
 
-void setup(){
-  
+void setup() {
+
+  // Configurar Timer 1 para PWM @ 25 kHz. min: 0, max: 320
+  TCCR1A = 0;           // undo the configuration done by...
+  TCCR1B = 0;           // ...the Arduino core library
+  TCNT1  = 0;           // reset timer
+  TCCR1A = _BV(COM1A1)  // non-inverted PWM on ch. A
+           | _BV(COM1B1)  // same on ch; B
+           | _BV(WGM11);  // mode 10: ph. correct PWM, TOP = ICR1
+  TCCR1B = _BV(WGM13)   // ditto
+           | _BV(CS10);   // prescaler = 1
+  ICR1   = 320;         // TOP = 320
+
+  // Set the PWM pins as output.
+  pinMode( 9, OUTPUT);
+  pinMode(10, OUTPUT);
+  //---- fin Configurar Timer 1 ---- //
+
   Serial.begin(9600);
   Serial.println("/*** sigue_linea_PD ***/");
   Serial.println("*** INITS ***");
   LEDsInit();
   MotoresInit(M1A, M2A);
   ApagarMotores(M1PWM, M1A, M2PWM, M2A);
-  //pinMode(BOTON,INPUT_PULLUP); // Boton para programa
+  pinMode(BOT_START, INPUT_PULLUP); // Boton para programa
+  pinMode(LLAVE, INPUT);           // Llave para elegir opcion
 
-  /*while(digitalRead(BOTON) == 1)
-  { 
-	LEDsBlink(1, 1, 1, 100);
-  }*/
+  while (digitalRead(BOT_START) == 1)
+  {
+    LEDsBlink(1, 1, 1, 100);
+  }
 
   ApagarMotores(M1PWM, M1A, M2PWM, M2A);
-  LEDsDrive(1,1);
-  
-  /*while(digitalRead(BOTON) == 1){}*/
-  
+  LEDsDrive(1, 1);
+
+  while(digitalRead(BOT_START) == 1){}
+
   Serial.println("*** GO! ***");
-  LEDsDrive(0,0);
-  delay(3000);
-  Flag = true;      // Para que inicie sin el boton
-  
+  LEDsDrive(0, 0);
+
 }
 
-void loop(){
-  
-  /*if(digitalRead(BOTON) == 0)
-  {
-   Flag = !Flag;
-   delay(200); 
-  }*/
-  
-  if(Flag == true)
+void loop() {
+
+  if(digitalRead(BOT_START) == 0)
+    {
+    Flag = !Flag;
+    delay(200);
+    }
+
+  if (Flag == true)
   {
     Now = micros();
-    dt = Now - *LastTime;     
+    dt = Now - *LastTime;
 
-    if(dt > Constantes[2])
+    if (dt > Constantes[2])
     {
       Vel_filtrada[0] = LecturaSensores2(1, 0, Error, LastError, LastTime2, Flags);
-      SeguirLinea(M1PWM, M1A, M2PWM, M2A, Vel_filtrada[0], Constantes[3], 0);  // Probar varios delays
+      SeguirLinea(M1PWM, M1A, M2PWM, M2A, Vel_filtrada[0], Constantes[3]);
       *LastTime = Now;
     }
-    
+
   }
   else
   {
-    LEDsDrive(0,0);
+    LEDsDrive(0, 0);
     ApagarMotores(M1PWM, M1A, M2PWM, M2A);
   }
-  
+
 }
--- src/seguidor/main/motores.ino
+++ src/seguidor/main/motores.ino
@@ -1,10 +1,10 @@
 
 
- /*
-    1 - MotoresInit
-    2 - MoverMotores
-    3 - ApagarMotores
-    4 - SeguirLinea
+/*
+   1 - MotoresInit
+   2 - MoverMotores
+   3 - ApagarMotores
+   4 - SeguirLinea
 */
 
 
@@ -20,26 +20,61 @@ void MotoresInit(byte motor1D, byte motor2D)
 }
 
 /*----------------------------------------------------------------------------------
-    Función para apagar los Motores con valores absolutos VelM1/2 : (-255; 255)
+    Función para usar PWM a 25 kHz en los pines 9 y 10 (timer 1). valor: entre 0 y 320
+  ----------------------------------------------------------------------------------*/
+
+void analogWrite25k(int pin, int valor)
+{
+  switch (pin) {
+    case 9:
+      OCR1A = valor;
+      break;
+    case 10:
+      OCR1B = valor;
+      break;
+    default:
+      // no other pin will work
+      break;
+  }
+}
+
+/*----------------------------------------------------------------------------------
+    Función para mover los Motores con valores absolutos VelM1/2 : (-320; 320)
   ----------------------------------------------------------------------------------*/
 
 void MoverMotores (byte motor1PWM, byte motor1D, byte motor2PWM, byte motor2D, int velocidadMotor1, int velocidadMotor2)
 {
-  if (velocidadMotor1 > 0)
-  {
-    analogWrite(motor1PWM, velocidadMotor1);
+  if (velocidadMotor1 >= PWM_DEAD_ZONE) {  // si estoy en zona lineal, muevo el motor linealmente
+    analogWrite25k(motor1PWM, velocidadMotor1);
     digitalWrite(motor1D, 0);
-  } else {
-    analogWrite(motor1PWM, 255 + velocidadMotor1);
+  }
+  else if (velocidadMotor1 <= -PWM_DEAD_ZONE) { // idem pero en sentido contrario
+    analogWrite25k(motor1PWM, 320 + velocidadMotor1);
+    digitalWrite(motor1D, 1);
+  }
+  else if (velocidadMotor1 > 0) { // si estoy en la banda muerta pero positiva, voy y lo muevo la cantidad mínima
+    analogWrite25k(motor1PWM, PWM_DEAD_ZONE);
+    digitalWrite(motor1D, 0);
+  }
+  else {                          // idem pero en sentido contrario
+    analogWrite25k(motor1PWM, 320 + PWM_DEAD_ZONE);
     digitalWrite(motor1D, 1);
   }
 
-  if (velocidadMotor2 > 0)
-  {
-    analogWrite(motor2PWM, velocidadMotor2);
+  if (velocidadMotor2 >= PWM_DEAD_ZONE) {  // si estoy en zona lineal, muevo el motor linealmente
+    analogWrite25k(motor2PWM, velocidadMotor2);
     digitalWrite(motor2D, 0);
-  } else  {
-    analogWrite(motor2PWM, 255 + velocidadMotor2);
+  }
+  else if (velocidadMotor2 <= -PWM_DEAD_ZONE) { // idem pero en sentido contrario
+    analogWrite25k(motor2PWM, 320 + velocidadMotor2);
+    digitalWrite(motor2D, 1);
+  }
+  else if (velocidadMotor1 > 0) { // si estoy en la banda muerta pero positiva, voy y lo muevo la cantidad mínima
+    analogWrite25k(motor2PWM, PWM_DEAD_ZONE);
+    digitalWrite(motor2D, 0);
+  }
+  else {
+    analogWrite25k(motor2PWM, 320 + PWM_DEAD_ZONE);
     digitalWrite(motor2D, 1);
   }
 }
@@ -49,8 +84,8 @@ void MoverMotores (byte motor1PWM, byte motor1D, byte motor2PWM, byte motor2D, i
   -------------------------------------------------------------*/
 void ApagarMotores(byte motor1PWM, byte motor1D, byte motor2PWM, byte motor2D)
 {
-  analogWrite(motor1PWM, 255);
-  analogWrite(motor2PWM, 255);
+  analogWrite25k(motor1PWM, 320);
+  analogWrite25k(motor2PWM, 320);
   digitalWrite(motor1D, 1);
   digitalWrite(motor2D, 1);
 }
@@ -59,44 +94,32 @@ void ApagarMotores(byte motor1PWM, byte motor1D, byte motor2PWM, byte motor2D)
     SeguirLinea; Mueve los motores seg�n la salida de LecturaSensores2.
 */
 
-void SeguirLinea(byte motor1PWM, byte motor1D, byte motor2PWM, byte motor2D, int VelFiltrada, int Vel, int delay_ms)
-{ 
+void SeguirLinea(byte motor1PWM, byte motor1D, byte motor2PWM, byte motor2D, int VelFiltrada, int Vel)
+{
 
-    int M1Vel = 0;
-    int M2Vel = 0;
+  int M1Vel = 0;
+  int M2Vel = 0;
 
+  M1Vel = Vel - VelFiltrada; // M1 Der. visto desde arriba y desde atrás.
+  M2Vel = Vel + VelFiltrada; // M2 Der. visto desde arriba y desde atrás..
 
-    //M1Vel = Vel-VelFiltrada+5; // M1 Izq. visto de arriba. Debo sumarle un poco más por diferencias de motores.
-    //M2Vel = Vel+VelFiltrada-13; // M2 Der. visto de arriba. Debo restarle un poco más por diferencias de motores.
+  if (M1Vel > 0)
+  {
+    LEDsDrive(1, 0);
+  }
+  else
+  {
+    LEDsDrive(0, 1);
+  }
 
-    M1Vel = Vel-VelFiltrada; // M1 Izq. visto de arriba. Debo sumarle un poco más por diferencias de motores.
-    M2Vel = Vel+VelFiltrada; // M2 Der. visto de arriba. Debo restarle un poco más por diferencias de motores.
-          
-        if(M1Vel > 0)
-        {
-          LEDsDrive(1,0);          
-        }
-        else
-        {
-          LEDsDrive(0,1); 
-        }
-        
-        //if(M1Vel > VEL_MAX){M1Vel = VEL_MAX; M2Vel = -VEL_MAX;}
-        //if(M2Vel > VEL_MAX){M2Vel = VEL_MAX; M1Vel = -VEL_MAX;}
-        //if(M1Vel < -VEL_MAX){M1Vel = -VEL_MAX;M2Vel = VEL_MAX;}
-        //if(M2Vel < -VEL_MAX){M2Vel = -VEL_MAX;M1Vel = VEL_MAX;}
-        
-        M1Vel = constrain(M1Vel,-VEL_MAX,VEL_MAX);
-        M2Vel = constrain(M2Vel,-VEL_MAX,VEL_MAX);
-        
-        MoverMotores(motor1PWM, motor1D, motor2PWM, motor2D, M1Vel, M2Vel); 
-		
-	delay(delay_ms);
-                                                               
-        
+  M1Vel = constrain(M1Vel, -VEL_MAX, VEL_MAX);
+  M2Vel = constrain(M2Vel, -VEL_MAX, VEL_MAX);
+
+  MoverMotores(motor1PWM, motor1D, motor2PWM, motor2D, M1Vel, M2Vel);
+
 }
 
 
- 
- 
- 
+
+
+
